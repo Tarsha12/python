2230aa7afed44e0e81eea323b8e00e6ef876dfd7
Tarsha12 <nsnatty@hotmail.com>
2025-07-30 23:05:55 -0700

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,21 @@
+print("Welcome to the world of coding")
+print(4)
+print("Hello Python \n")
+print("welcome to ", end= "*")
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+codingal = "Codingal"
+print(codingal)
+
+name = input("enter your name: ")
+print("\nHello", name, "\nwelcome to codingal")
+
+import keyword
+
+# print all the Python keywords
+print ("Python keywords are...\n")
+print(keyword.kwlist)
