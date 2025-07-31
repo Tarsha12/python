eedf345b2ebd5b3c4753858d4453187e81e2ad16
Tarsha12 <nsnatty@hotmail.com>
2025-07-30 23:23:24 -0700

    Add files via upload

---
+++ p1acp.py
@@ -0,0 +1,11 @@
+mum = "51/09/2000"
+dad = "38/85/9632"
+grandpa  = "14/95/1504"
+grandma = "36/94/2550"
+grandpapa = "29/95/0649"
+
+print("my mum's birthday is the",mum)
+print("my dad's birthday is the",dad)
+print("my grandpa's birthday is the",grandpa)
+print("my grandma's birthday is the",grandma)
+print("Grandpapa's birthday is the",grandpapa)
